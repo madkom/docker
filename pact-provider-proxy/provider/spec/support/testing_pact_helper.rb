9327b95a5f413bdd70fa8672646bc2e8d05d9063
Darek Gafka <d.gafka@madkom.pl>
2015-11-17 13:51:06 +0100

    Added Pact Provider Proxy

---
+++ pact-provider-proxy/provider/spec/support/testing_pact_helper.rb
@@ -0,0 +1,9 @@
+Pact.provider_states_for "Alligator Consumer" do
+
+  provider_state "an alligator with the name Mary exists" do
+    set_up do
+      # Your set up code goes here
+    end
+  end
+
+end
